d4f96d13c34b1edc285f9c20c45ff139cd4f7775
Icaro Ferreira <icrfcc@gmail.com>
2023-03-11 19:12:02 -0300

    Finished the logic for the super and the not very effective attacks based on Pokemon and moves types

--- functions.js
+++ functions.js
@@ -1,117 +1,107 @@
 //Knight or Sorcerer
 //LittleMonster or BigMonster
 
-const player1PokemonsList = [
+const defaultPokemonStatus = {
+    life: 100,
+    maxLife: 100,
+    attack: 10,
+    defense: 4
+}
+
+const pokemonsList = [
     {
+        ...defaultPokemonStatus,
         name: 'Pikachu',
-        life: 100,
-        maxLife: 100,
-        attack: 20,
-        defense: 6,
         type: 'eletric',
-        strongAgainst: ['water'],
-        weakAgainst: ['ground', 'grass']
+        moves: [
+            {name: 'thunderbolt', type: 'eletric', strengthness: ['water', 'flying'], weakness: ['ground', 'grass']},
+            {name: 'quick attack', type: 'normal', strengthness: [''], weakness: ['flying']}
+        ]
     },
     
     {
+        ...defaultPokemonStatus,
         name: 'Bulbasaur',
-        life: 100,
-        maxLife: 100,
-        attack: 16,
-        defense: 6,
         type: 'grass',
-        strongAgainst: ['ground', 'water'],
-        weakAgainst: ['poison', 'fire']
+        moves: [
+            {name: 'razor leaf', type: 'grass', strengthness: ['ground', 'water', 'rock'], weakness: ['poison', 'fire', 'bug', 'flying']},
+            {name: 'cut', type: 'normal', strengthness: [''], weakness: ['flying']}
+        ]
     },
     
     {
+        ...defaultPokemonStatus,
         name: 'Charmander',
-        life: 100,
-        maxLife: 100,
-        attack: 18,
-        defense: 5,
         type: 'fire',
-        strongAgainst: ['poison', 'grass'],
-        weakAgainst: ['ground', 'water']
+        moves: [
+            {name: 'flamethrower', type: 'fire', strengthness: ['bug', 'poison', 'grass', 'steel'], weakness: ['ground', 'water', 'rock']},
+            {name: 'slash', type: 'normal', strengthness: [''], weakness: ['flying']}
+        ]
     },
 
     {
+        ...defaultPokemonStatus,
         name: 'Squirtle',
-        life: 100,
-        maxLife: 100,
-        attack: 17,
-        defense: 7,
         type: 'water',
-        strongAgainst: ['ground'],
-        weakAgainst: ['grass', 'water']
-    }
-];
+        moves: [
+            {name: 'surf', type: 'water', strengthness: ['ground', 'fire', 'rock'], weakness: ['grass', 'eletric']},
+            {name: 'body slam', type: 'normal', strengthness: [''], weakness: ['flying']}
+        ]
+    },
 
-
-
-// Enemy Pokémons bellow
-
-const player2PokemonsList = [
     {
+        ...defaultPokemonStatus,
         name: 'Ekans',
-        life: 100,
-        maxLife: 100,
-        attack: 16,
-        defense: 4,
-        type: 'poison'
+        type: 'poison',
+        moves: [
+            {name: 'earthquake', type: 'ground', strengthness: ['eletric', 'fire', 'poison', 'rock', 'steel'], weakness: ['grass', 'water', 'ice']},
+            {name: 'acid', type: 'poison', strengthness: ['fairy', 'grass'], weakness: ['ground', 'psychic']}
+        ]
     },
     
     {
+        ...defaultPokemonStatus,
         name: 'Horsea',
-        life: 100,
-        maxLife: 100,
-        attack: 17,
-        defense: 3,
-        type: 'water'
+        type: 'water',
+        moves: [
+            {name: 'hydro pump', type: 'water', strengthness: ['ground', 'fire', 'rock'], weakness: ['grass', 'eletric']},
+            {name: 'ice beam', type: 'ice', strengthness: ['dragon', 'flying', 'grass', 'ground'], weakness: ['fighting', 'fire', 'rock', 'steel']}
+        ]
     },
     
     {
+        ...defaultPokemonStatus,
         name: 'Bellsprout',
-        life: 100,
-        maxLife: 100,
-        attack: 15,
-        defense: 3,
-        type: 'grass'
+        type: 'grass',
+        moves: [
+            {name: 'razor leaf', type: 'grass', strengthness: ['ground', 'water', 'rock'], weakness: ['poison', 'fire', 'bug', 'flying']},
+            {name: 'mega drain', type: 'grass', strengthness: ['ground', 'water', 'rock'], weakness: ['poison', 'fire', 'bug', 'flying']}
+        ]
     },
 
     {
-        name: 'Mewtwo',
-        life: 500,
-        maxLife: 500,
-        attack: 28,
-        defense: 10,
-        type: 'normal'
+        ...defaultPokemonStatus,
+        name: 'Sandslash',
+        type: 'ground',
+        moves: [
+            {name: 'dig', type: 'ground', strengthness: ['eletric', 'fire', 'poison', 'rock', 'steel'], weakness: ['grass', 'water', 'ice']},
+            {name: 'sand attack', type: 'normal', strengthness: [], weakness: ['flying']}
+        ]
     }
 ];
 
 const charactersSetter = (player1Index) => {
+
+    let player1;
+    let player2;
         
-        let player2Index = Math.floor((Math.random() * 4));
+        do {
+            let player2Index = Math.floor((Math.random() * 4));
 
-        let player1 = player1PokemonsList[player1Index];
-        let player2 = player2PokemonsList[player2Index];
-
-        if (player1.strongAgainst.includes(player2.type)) {
-            console.log('Player 1 attack first: ', player1.attack);
-            player1.attack *= 2;
-            console.log('Player1 attack has been increased by 2');
-            console.log('Player 1 attack after: ', player1.attack);
-
+            player1 = pokemonsList[player1Index];
+            player2 = pokemonsList[player2Index];     
             
-        }
-            
-        else if (player1.weakAgainst.includes(player2.type)) {
-                console.log('Player 2 attack first: ', player2.attack);
-                player2.attack *= 2;
-                console.log('Player2 attack has been increased by 2');
-                console.log('Player 2 attack after: ', player2.attack);
-                
-            }
+        } while (player1 === player2);   //So a Pokemon won't battle against itself
                 
         
         return [player1, player2];
@@ -129,15 +119,40 @@ const stage = {
 
     start(fighter1, fighter2, fighter1Element, fighter2Element) {
         this.fighter1 = fighter1;
-        this.fighter2 = fighter2;
         this.fighter1Element = fighter1Element;
-        this.fighter2Element = fighter2Element;
+        this.fighter1Move1 = this.fighter1Element.querySelector('.move1');
+        this.fighter1Move2 = this.fighter1Element.querySelector('.move2');
+
+        this.fighter2 = fighter2;        
+        this.fighter2Element = fighter2Element;        
+        this.fighter2Move1 = this.fighter2Element.querySelector('.move1');
+        this.fighter2Move2 = this.fighter2Element.querySelector('.move2');
+
+
+
 
         this.fighter1Element.querySelector('.fighter-img').style.backgroundImage = `url('./Images/${fighter1.name}.png')`;
-        this.fighter2Element.querySelector('.fighter-img').style.backgroundImage = `url('./Images/${fighter2.name}.png')`;
+        this.fighter1Move1.innerHTML = fighter1.moves[0].name;
+        this.fighter1Move2.innerHTML = fighter1.moves[1].name;
+        this.fighter1Move1.setAttribute('type', fighter1.moves[0].type);
+        this.fighter1Move2.setAttribute('type', fighter1.moves[1].type);
 
-        this.fighter1Element.querySelector('.attackButton').addEventListener('click', ()=> this.doAttack(this.fighter1, this.fighter2));
-        this.fighter2Element.querySelector('.attackButton').addEventListener('click', ()=> this.doAttack(this.fighter2, this.fighter1));
+        this.fighter2Element.querySelector('.fighter-img').style.backgroundImage = `url('./Images/${fighter2.name}.png')`;
+        this.fighter2Move1.innerHTML = fighter2.moves[0].name;
+        this.fighter2Move2.innerHTML = fighter2.moves[1].name;
+        this.fighter2Move1.setAttribute('type', fighter2.moves[0].type);
+        this.fighter2Move2.setAttribute('type', fighter2.moves[1].type);
+
+
+        //Change it here, for the different types of moves...
+        // this.fighter1Element.querySelector('.attackButton').addEventListener('click', ()=> this.doAttack(this.fighter1, this.fighter2));
+        // this.fighter2Element.querySelector('.attackButton').addEventListener('click', ()=> this.doAttack(this.fighter2, this.fighter1));
+
+        this.fighter1Move1.addEventListener('click', ()=> this.doAttack(this.fighter1, this.fighter2, fighter1.moves[0]));
+        this.fighter1Move2.addEventListener('click', ()=> this.doAttack(this.fighter1, this.fighter2, fighter1.moves[1]));
+
+        this.fighter2Move1.addEventListener('click', ()=> this.doAttack(this.fighter2, this.fighter1, fighter2.moves[0]));
+        this.fighter2Move2.addEventListener('click', ()=> this.doAttack(this.fighter2, this.fighter1, fighter2.moves[1]));
     
         this.update();
 
@@ -162,8 +177,10 @@ const stage = {
         this.fighter2Element.querySelector('.bar').style.width = `${fighter2PercentageLife}%`;
     },
 
-    doAttack(attacking, attacked) {
-        // console.log(`${attacking.name} atacou ${attacked.name}`);
+    doAttack(attacking, attacked, move) {
+        console.log(`${attacking.name} atacou ${attacked.name}`);
+        console.log('Move chosen: ', move);
+        console.log('Attacked type: ', attacked.type);
 
         if (attacking.life <= 0 || attacked.life <= 0) {
             console.log('Someone is already dead! The battle is over.');
@@ -171,18 +188,36 @@ const stage = {
             return;
         }
 
-        let attackFactor = (Math.random() * 2).toFixed(2);
+        // if (attacking.moves)   check if the attacking.moveChosenStrong includes the attaacked type -> attacking.Attack = attaching.attack * 1.5 (super effective)
+        //else if the attacking.moveChosenWeakness includes the attaacked type -> attacking.Attack = attaching.attack * 0.3 (not very effective)
+
+            
+
+        let attackFactor = (Math.random() * 1) + 1;
         console.log('Attack factor: ', attackFactor);
-        let defenseFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 1) + 1;
         //The final attack and defense power can be up to double of themselves
 
-        const actualAttack = attacking.attack * attackFactor;
-        const actualDefense = attacked.defense * defenseFactor;
-        const damage = (actualAttack - actualDefense).toFixed(1);
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+        
 
+        if (move.strengthness.includes(attacked.type)) {
+            actualAttack = actualAttack * 1.5;
+            console.log("It's super effective!!!");
+        }
+            
+        else if (move.weakness.includes(attacked.type)) {
+            actualAttack = actualAttack * 0.5;
+            console.log("It's not very effective...");
+        }
+                
+
+        let damage = Math.round((actualAttack - actualDefense));
         console.log('Attack power: ', actualAttack);
         console.log('Defense power: ', actualDefense);
         console.log('Damage: ', damage);
+
 
         if (damage > 0) {
             attacked.life -= damage;
